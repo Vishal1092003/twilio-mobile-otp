97b020b4ca81ce849fa20860894b8a07cb06c7a4
vishal <codewithme2615@gmail.com>
2025-07-13 18:12:10 +0530

    twilio

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express= require("express")
 const app=express()
 
 
-const accountSid = 'accountSid';
-const authToken = 'authToken';
+const accountSid = '';
+const authToken = '';
 
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
-    body: 'Hello world 1234',
-    to: '+919874561230', // Text your number
-    from: '+12562861804', // From a valid Twilio number
+    body: 'hack kar lengein',
+    to: '+917292994406', // Text your number
+    from: '+15183270051', // From a valid Twilio number
   })
   .then((message) => console.log(message.sid));
 
